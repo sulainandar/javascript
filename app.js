5fbff4ae649299fbd108ecb41bee8325375331d5
sulainandar <sulai.chitsu@gmail.com>
2025-09-07 14:54:35 +0900

    first changes

---
+++ app.js
@@ -0,0 +1,9 @@
+let age = 19;
+
+let ageChecked = (age>18) ? console.log("You can drive") : console.log("You cannot drive");
+
+let header = document.getElementById("header");
+header.style.color = "red";
+
+let content = document.querySelectorAll(".content");
+console.log(content);
